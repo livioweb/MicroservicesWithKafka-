fe3f19d537de9dab5d7d6ef4b409c8d43d8d5345
LIVIO RODRIGUES LOPES <livio.lopes@armateus.com.br>
2020-11-27 07:43:01 -0300

    D - MS/KAFKA

---
+++ apps/consumer/consumer_sum.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const Kafka = require('no-kafka');
+
+var valueSum = 0;
+
+// Create an instance of the Kafka consumer
+const consumer = new Kafka.SimpleConsumer(
+    {
+            "connectionString":"127.0.0.1:9092"
+        }
+    )
+var data = function (messageSet) {
+    messageSet.forEach(function (m) {
+        var value = parseInt(m.message.value.toString('utf8'));
+        valueSum = valueSum + value;
+        console.log(valueSum);
+    });
+};
+
+// Subscribe to the Kafka topic
+return consumer.init().then(function () {
+    return consumer.subscribe('LOJA_NOVO_PEDIDO', data);
+});
